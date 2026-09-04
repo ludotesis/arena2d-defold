3ac8e418ce161fbdf9350e217983ac78be8e847e
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-09-04 16:07:58 -0300

    GO Samurai
    GameObject Samurai
    Atlas de Enemigos
    Sprites Samurai
    Camera en MainCollection

---
+++ ARENA 2D/go/jugador.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"corrrer\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/atlas/jugador.atlas\"\n"
+  "}\n"
+  ""
+}
---
+++ ARENA 2D/go/samurai.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"samurai_correr\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/atlas/enemigos.atlas\"\n"
+  "}\n"
+  ""
+}
