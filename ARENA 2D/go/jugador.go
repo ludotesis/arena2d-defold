57f33d9e866769c730b64a786ad145f58d6db76f
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-09-04 16:30:26 -0300

    Jugador Script
    Movimientos del Jugador en 4 direcciones
    Input config: Arriba, Abajo, Izquierda y Derecha

--- ARENA 2D/go/jugador.go
+++ ARENA 2D/go/jugador.go
@@ -1,3 +1,7 @@
+components {
+  id: "jugador"
+  component: "/scripts/jugador.script"
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
@@ -6,6 +10,31 @@ embedded_components {
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
   "  texture: \"/atlas/jugador.atlas\"\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 8.0\n"
+  "  data: 8.0\n"
+  "  data: 0.5\n"
   "}\n"
   ""
 }
