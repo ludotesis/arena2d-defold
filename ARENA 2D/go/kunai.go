499a4ae923aaadbfbf89556783ec533ad5803743
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-09-15 16:30:13 -0300

    GO Kunai

---
+++ ARENA 2D/go/kunai.go
@@ -0,0 +1,40 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"Kunai\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/atlas/jugador.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.5
+    y: 0.5
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"armas\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 1.0\n"
+  "      y: 1.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 1.75\n"
+  "}\n"
+  ""
+}
